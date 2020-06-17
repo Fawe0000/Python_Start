83633702a18fdf8a92eb1076517756fdd3c0d6bd
Alex.V <fawe@inbox.ru>
2020-06-17 03:04:51 +0300

    Lesson2 Homework 1 - 5

--- Lesson2/Lesson_2_5.py
+++ Lesson2/Lesson_2_5.py
@@ -1,15 +1,6 @@
 start_list = [8, 7, 5, 3, 3, 2]
 print(f"В настоящий момент рейтинг такой: {start_list}")
 new_n = int(input("Введите новое значение: "))
-end_list = []
-for enum, el in enumerate(start_list):
-    print(enum, el, new_n)
-    if new_n > el:
-
-        print(end_list.insert(enum, new_n))
-        print(f"if {end_list}")
-    else:
-        end_list.insert(enum, el)
-        print(f"else {end_list}")
-
-print(end_list)
+start_list.append(new_n)
+start_list = sorted(start_list, reverse=True)
+print(f"Рейтинг, включая введенное значение: {start_list}")
