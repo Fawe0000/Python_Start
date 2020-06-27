344494756c9b3597a098d78e30b0cfe62bb2ceb3
Alex.V <fawe@inbox.ru>
2020-06-27 18:44:12 +0300

    Homework Lesson 5_4

--- Lesson_5_4.py
+++ Lesson_5_4.py
@@ -1,12 +1,22 @@
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
- ещё строка
+try:
+    with open("text_4.txt", "r", encoding="utf-8") as my_file:
+        new_file = open("text_4w.txt", "w+", encoding="utf-8")
+        my_chisl = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть','семь','восемь','девять']
+        new_str = []
+        k_str = 0
+        summ_okl = 0
+        prem = 0
+        list_str = []
+        for line in my_file:
+            list_str = line.split(' ')
+            prem = int(list_str[2])
+            list_str[0] = my_chisl[prem]
+            print(f" {' '.join(list_str)}", file=new_file)
+            k_str += 1
+            summ_okl += prem
+except IOError:
+    print("Error")
+else:
+    new_file.seek(0)
+    print(f'Содерджимое файла "{new_file.name}:')
+    print(new_file.read())
